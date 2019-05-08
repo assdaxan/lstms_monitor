62d59acc3746f720af44b62ce2f394b61726b0dd
assdaxan <elaalsdn1@naver.com>
2019-05-08 22:59:35 +0900

    트래픽 코드 수정

--- module/GetNetworkTraffic.py
+++ module/GetNetworkTraffic.py
@@ -1,30 +1,27 @@
 from time import sleep
-from . import GetNetInterface
 
 class GetNetworkTraffic:
-    def __init__(self):
-        RX_PATH = '/sys/class/net/%s/statistics/rx_bytes'
-        TX_PATH = '/sys/class/net/%s/statistics/tx_bytes'
-
-        netinter = GetNetInterface()
-        self.interface_dict = netinter.Get()
+    def __init__(self, interface_name):
+        self.interface_name = interface_name
+        self.RX_PATH = f'/sys/class/net/{self.interface_name}/statistics/rx_bytes'
+        self.TX_PATH = f'/sys/class/net/{self.interface_name}/statistics/tx_bytes'
 
     def Get(self):
-        def get(interface_name):
+        def get(PATH):
             with open(PATH) as f:
                 return int(f.read(), 10)
 
-        for interface_name in self.interface_dict.keys():
-            before_rx = get(RX_PATH%interface_name)
-            before_tx = get(TX_PATH%interface_name)
-            sleep(3)
-            after_rx = get(RX_PATH%interface_name)
-            avter_tx = get(TX_PATH%interface_name)
+        before_rx = get(self.RX_PATH)
+        before_tx = get(self.TX_PATH)
+        sleep(3)
+        after_rx = get(self.RX_PATH)
+        after_tx = get(self.TX_PATH)
 
-            print(after_rx - before_rx)
-            print(after_tx - after_tx)
+        print(self.interface_name)
+        print(after_rx - before_rx)
+        print(after_tx - before_tx)
 
 
 if __name__ == "__main__":
-    t = GetNetworkTraffic()
+    t = GetNetworkTraffic('wlp0s20f3')
     t.Get()
