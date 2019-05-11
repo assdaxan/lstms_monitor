caf8e0265056c0f4bd2466560101436a95d202ed
assdaxan <elaalsdn1@naver.com>
2019-05-11 20:26:25 +0900

    정규 표현식 그룹 이름 추가

--- module/GetNetInterface.py
+++ module/GetNetInterface.py
@@ -5,8 +5,8 @@ import re
 class GetNetInterface(Shell):
     def __init__(self):
         self.re_interface_info = re.compile(r'((\w+)(?:: )(?:.*)(?:(?:\n {8}).*)*)')
-        self.re_interface_inet = re.compile(r'(inet)(?: *)((?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))')
-        self.re_interface_inet6 = re.compile(r'(?:inet6)(?: *)((?:(?:[a-f0-9]{0,4}):?){6})')
+        self.re_interface_inet = re.compile(r'(inet)(?: *)(?P<address>(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))')
+        self.re_interface_inet6 = re.compile(r'(?:inet6)(?: *)(?P<address>(?:(?:[a-f0-9]{0,4}):?){6})')
 
     def Get(self):
         interface_dict = {}
@@ -15,11 +15,11 @@ class GetNetInterface(Shell):
         for data in self.re_interface_info.findall(t):
             interface_name = data[1]
             try:
-                interface_inet = self.re_interface_inet.search(data[0]).group(2)
+                interface_inet = self.re_interface_inet.search(data[0]).group('address')
             except AttributeError:
                 interface_inet = ''
             try:
-                interface_inet6 = self.re_interface_inet6.search(data[0]).group(1)
+                interface_inet6 = self.re_interface_inet6.search(data[0]).group('address')
             except AttributeError:
                 interface_inet6 = ''
             interface_dict.update({interface_name:{'inet4':interface_inet, 'inet6':interface_inet6}})
