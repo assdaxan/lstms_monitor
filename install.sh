433f0adddeabf61cc590cd7d5508d38446d4e600
assdaxan <elaalsdn1@naver.com>
2019-06-11 21:51:55 +0900

    설치코드 수정

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo apt install -y python3
 echo "Add User lstms_m"
 echo 'Y' | sudo adduser --shell /usr/sbin/nologin --no-create-home --disabled-password --disabled-login --quiet lstms_m
 
+echo "LSTMS Permission Setting"
+chown -R lstms_m:lstms_m /etc/lstms_m
+
 echo "Register LSTMS Client"
 sudo /usr/bin/python3 /etc/lstms_m/lstms-register.py
 
@@ -18,7 +21,10 @@ echo "Add Crontab"
 (sudo crontab -u lstms_m -l; echo '* * * * * /usr/bin/python3 /etc/lstms_m/lstms-user.py' ) | sudo crontab -u lstms_m -
 
 echo "Add Systemd Service"
-sudo cp /etc/lstmsm_m/lstms.service /etc/systemd/system/lstms.service
+sudo cp /etc/lstms_m/lstms.service /etc/systemd/system/lstms.service
+
+echo "Enable LSTMS Service"
+sudo systemctl enable lstms
 
 echo "Start LSTMS Service"
 sudo systemctl start lstms
